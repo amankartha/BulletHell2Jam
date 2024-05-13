a36589e083d3126c29f714a98c0a19ea3decbdea
Aman Kartha <amankartha1998@gmail.com>
2024-05-12 22:33:33 -0400

    save

---
+++ Assets/Game/Scripts/System/Door.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public int Health;
+    public GameObject[] EnemiesToDestroy;
+    public MMF_Player DeathFeel;
+    public GameObject NextCameraPosition;
+    
+
+}
