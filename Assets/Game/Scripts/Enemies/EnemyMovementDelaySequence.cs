ea971ac4ddfb90e00b331c78b87b42ebaff57e96
Aman Kartha <amankartha1998@gmail.com>
2024-05-15 13:02:56 -0400

    waypoint cahnge

--- Assets/Game/Scripts/Enemies/EnemyMovementDelaySequence.cs
+++ Assets/Game/Scripts/Enemies/EnemyMovementDelaySequence.cs
@@ -6,16 +6,32 @@ using UnityEngine;
 public class EnemyMovementDelaySequence : MonoBehaviour
 {
     private DOTweenPath _doTweenPath;
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        _doTweenPath = this.gameObject.GetComponent<DOTweenPath>();
+        transform.DOPath(_doTweenPath.path.wps, _doTweenPath.duration).SetLoops(-1).OnWaypointChange(Pause);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void Pause(int i)
+    {
+        transform.DOPause();
+   
+        Invoke(nameof(Resume), _doTweenPath.delay);
+    }
+
+    private void Resume()
+    {
+        transform.DOPlay();
     }
 }
+    
+    
