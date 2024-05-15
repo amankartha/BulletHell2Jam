987c6f1ee680afc7c8e1efdeac446a65bf76f8ae
Aman Kartha <amankartha1998@gmail.com>
2024-05-15 15:36:53 -0400

    shitty charging placeholder

--- Assets/Game/Scripts/Interactables/ChargingPad.cs
+++ Assets/Game/Scripts/Interactables/ChargingPad.cs
@@ -6,14 +6,36 @@ using UnityEngine;
 public class ChargingPad : MonoBehaviour
 {
     [SerializeField] private float HeatChargePerSecond = 5f;
-    
-   
+    private ParticleSystem PS;
+
+    private void Start()
+    {
+        PS = this.gameObject.GetComponentInChildren<ParticleSystem>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject == GameManager.Instance.MAINPLAYERGAMEOBJECT)
+        {
+            PS.Play();
+        }
+    }
+
     //TODO: only add charge when in movingState
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject == GameManager.Instance.MAINPLAYERGAMEOBJECT)
         {
             GameManager.Instance.MAINPLAYERSCRIPT.Heat += 5 * Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == GameManager.Instance.MAINPLAYERGAMEOBJECT)
+        {
+            PS.Stop();
         }
     }
 }
